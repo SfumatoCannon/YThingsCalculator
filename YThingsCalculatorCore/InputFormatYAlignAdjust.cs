8dc6cb723bea5c04649762420d15e513bbe3da9e
SfumatoCannon <3075964583@qq.com>
2025-12-18 23:42:52 +0800

    add InputFormatYAlignAdjust

---
+++ YThingsCalculatorCore/InputFormatYAlignAdjust.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YThingsCalculatorCore
+{
+    public class InputFormatYAlignAdjust
+    {
+        float StartY { get; set; }
+        float TargetYRangeLeft { get; set; }
+        float TargetYRangeRight { get; set; }
+
+        enum HeightLimitType
+        {
+            Null,
+            Solid,
+            Deadly
+        };
+        HeightLimitType HeightLimit { get; set; }
+
+        enum SearchStrategyType
+        {
+            Easiest,
+            Fastest
+        };
+        SearchStrategyType SearchStrategy { get; set; }
+
+        bool CanCactus { get; set; } // 定义仙人掌跳为 "第一次取消为非一帧跳的多重取消跳跃"
+        bool CanJumpCancel { get; set; } // 定义JC跳为 "第一次取消为一帧跳的多重取消跳跃"
+        bool IsPlatformLanding { get; set; }
+        bool CanDoubleJump { get; set; }
+        bool CanBunnyHop { get; set; }
+    }
+}
