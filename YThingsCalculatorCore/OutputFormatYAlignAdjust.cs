c4f64f118667dce7d34f221d7a1b1037cfbbe5a5
SfumatoCannon <3075964583@qq.com>
2025-12-18 20:16:26 +0800

    OutputFormatYAlignAdjust::GetOutputExpression

--- YThingsCalculatorCore/OutputFormatYAlignAdjust.cs
+++ YThingsCalculatorCore/OutputFormatYAlignAdjust.cs
@@ -10,6 +10,17 @@ namespace YThingsCalculatorCore
         float StartY { get; set; }
         float EndY { get; set; }
         List<LeapFormat> LeapList { get; set; } = [];
-        List<float> YProgressList { get; set; } = [];
+        List<float> YAlignProgressList { get; set; } = [];
+        String GetOutputExpression()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Start y: {StartY}\n");
+            int size = LeapList.Count;
+            for (int i = 0; i < size; i++)
+            {
+                sb.Append($"{LeapList[i].GetLeapExpression()} → {((double)YAlignProgressList[i]).ToString("R")}\n");
+            }
+            return sb.ToString();
+        }
     }
 }
