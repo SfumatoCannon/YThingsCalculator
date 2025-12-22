686a5e2136bdd3c3e940cd3ae99a5a62b308b277
SfumatoCannon <3075964583@qq.com>
2025-12-22 15:28:27 +0800

    add GetFrameY method(temp)

--- YThingsCalculatorCore/LeapSimulation.cs
+++ YThingsCalculatorCore/LeapSimulation.cs
@@ -6,5 +6,9 @@ namespace YThingsCalculatorCore
 {
     internal class LeapSimulation
     {
+        static float GetFrameY(float startY, LeapFormat leap, int frameIndex) { return 0.0f; /*WIP*/}
+        static float GetFrameY(float startY, string leapExpression, int frameIndex)
+        { return GetFrameY(startY, new LeapFormat(leapExpression), frameIndex); }
+
     }
 }
